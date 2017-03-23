08b73f37a027710c22b212ff61d8a7fb433aefc9
Brandon Cornett <brandon@cornett.ventures>
2017-03-23 00:36:15 -0500

    adds developer exception page

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,25 @@
+using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 
 namespace CoreDemo
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app) 
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) 
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseFileServer(enableDirectoryBrowsing: true);
 
             app.Run(context =>
             {
+                throw new Exception("Nah.");
+
                 return context.Response.WriteAsync("Hello World from the web!");
             });
         }
