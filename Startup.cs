4e95f3d66bacea5f029cf6ae36b02263ea7b5426
Brandon Cornett <brandon@cornett.ventures>
2017-03-23 08:24:47 -0500

    adds CORS handling

--- Startup.cs
+++ Startup.cs
@@ -3,14 +3,28 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CoreDemo
 {
     public class Startup
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+        }
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) 
         {
             app.UseErrorHandling();
+
+            app.UseCors(options => 
+            {
+                options.AllowAnyHeader();
+                options.AllowAnyMethod();
+                options.AllowAnyOrigin();
+                options.AllowCredentials();
+            });
 
             app.UseRewriter(new RewriteOptions()
                 .AddRewrite("rewrite", "foo/", true)
